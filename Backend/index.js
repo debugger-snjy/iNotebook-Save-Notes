139fe282cda67f8ebbe024b5955f14a2bd1668df
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2023-06-22 23:43:24 +0530

    Video 43 - Completed Added Express Code in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,21 @@
 // It is a Express JS File
 
+// Adding the Express JS Code :
+const express = require('express')
+
 // Now, Importing the connectToMongo function from "db.js"
 const connectToMongo = require('./db');
 
 // Checking for the connection
 connectToMongo()
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Sanjay!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
