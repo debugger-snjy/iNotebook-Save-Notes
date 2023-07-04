8d7c79c27d796d444044b5e8ddf63935da8ae279
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2023-07-04 23:32:44 +0530

    Video 49 - Completed - Added JWT Secret Key in .env file and used it using dotenv package - Also signing the JWT Token and sending user id to the user - We can verify the JWT Token using the verify method but we will create another endpoint for that [Line 105,106]

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -15,6 +15,21 @@ const { body, validationResult } = require('express-validator');
 // Importing bcryptjs for hashing, salt in password
 const bcrypt = require('bcryptjs');
 
+// Importing dotenv for accessing the Environment Variables
+const dotenv = require('dotenv');
+
+// Loads .env file contents into process.env by default
+dotenv.config();
+
+// Creating JWT_SECRET :
+// const JWT_SECRET = "Sanjayisagoodcoderok";
+// This should be hidden and not be disclosed
+// Putting it in .env.local
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+// Importing jsonwebtoken
+const jwt = require("jsonwebtoken");
+
 // Also, we have to change the get request to post request
 // We will add the validations after the endpoint in the post method
 // Here, no login required
@@ -56,13 +71,43 @@ router.post('/createuser', [
         const salt = await bcrypt.genSalt(10);
         // Generating a secure password & using await as it will return a promise and we have to wait until we get the results
         const securePass = await bcrypt.hash(req.body.password,salt);
+        
+        // Getting the Data of the user
         let userData = await User.create({
             name: req.body.name,
             // password: req.body.password, ==> Removing the Password as simple text and saving it in form of Hash
             password: securePass,
             email: req.body.email
         })
-        return res.json(userData)
+        
+        // We have successfully create a user, we don't return the details
+        // We will return some token which will be helpful in login verification
+        // Tokens are of 2 Types : Session Tokens and Json Web Token(JWT)
+        // We are using JWT authentication
+        // For that, we have a package in nodejs, which is "jsonwebtoken"
+        // Visit "jwt.io" for the example of JWT
+        // JWT contains 3 parts : 
+        //      1. Header
+        //      2. Payload
+        //      3. Signature
+        
+        // As we have got the userData, we will only save id of the user in the form of JSON
+        // and then it will be passed to the jwt and get signed!
+        const userID_Data = {
+            user : {
+                id : userData.id,
+            }
+        }
+        
+        // Signing the json web token
+        const authToken = jwt.sign(userID_Data,JWT_SECRET);
+
+        // Verifying the JWT Token but it will be needed in another endpoint like login endpoint
+        // console.log(jwt.verify(authToken,JWT_SECRET))
+        
+        // return res.json(userData)
+        return res.json({authToken})
+        // {authToken} is same as {"authToken" : authToken}
 
         // removing the then and catch as we have remove the promise
         // Also, Earlier when any error occurs, we only send message "Account already exists with this Email ID."
