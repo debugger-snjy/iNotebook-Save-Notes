91df6045b24200926ebc70c80c61c9a9d8ff007c
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2023-07-05 14:28:41 +0530

    Video 52 : fetchallnotes Endpoint - Importing the notes schema from Notes Mongoose Model - Importing the Middleware File - Also Creating fetchallnotes endpoint

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -6,9 +6,20 @@ const express = require('express');
 // Create a router of the express
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    console.log("Here, you can see your notes")
-    res.json([])
+// Importing the notes schema from User Mongoose Model
+const Notes = require("../models/Notes");
+
+// Importing the Middleware File :
+const fetchUser = require("../middleware/fetchUserId")
+
+// Route 1 : Fetching all the Notes of the User using GET Request "/api/notes/fetchallnotes"
+// Here, Login is Required ==> Middleware needed
+router.get('/fetchallnotes', fetchUser , async (req,res)=>{
+    
+    // Finding all the notes 
+    // Also to be specific, we have to add the user in the parameter to find notes of only that user
+    const notes = await Notes.find({user : req.user.id});
+    res.json(notes);
 })
 
 module.exports = router
