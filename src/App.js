c1fb0abc4b5c89a8123eb3a852c369fc28eef81d
Sanjay Sukhwani <sanjaysukhwani20@gnu.ac.in>
2023-07-06 22:25:35 +0530

    Video 56 - Completed - Added Routes in the App using react-router-dom - Using BrowserRouter in index.js - Using Route and Routes in the App.js - Created Components like Navbar, About, Home - Coded the Navbar Components and Links About and Home Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
 import './App.css';
+
+// Importing Route and Routes for the Routing
+import { Route, Routes } from "react-router-dom";
+import Home from './Components/Home';
+import Navbar from './Components/Navbar';
+import About from './Components/About';
 
 function App() {
     return (
-        <div className="App">
-            <h1>This is iNotebook</h1>
-        </div>
+        <>
+
+            {/* Adding Navigation Bar */}
+            <Navbar />
+
+            {/* Adding and Setting the Routers */}
+            <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/about' element={<About />} />
+            </Routes>
+
+        </>
     );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <div className="container">
+                <h1 className='text-center mt-3'>iNotebook - Save Notes Easily</h1>
+            </div>
+        </>
+    )
+}
